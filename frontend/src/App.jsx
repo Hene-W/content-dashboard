69bc97366ee489cb699d9af70f40b82a5cc77595
Hene-W <wannes.hene@gmail.com>
2026-06-24 11:14:19 +0100

    chore: initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-2">Dashboard Interne - Gestion de Contenu</h1>
+    </div>
+  )
+}
+
+export default App
